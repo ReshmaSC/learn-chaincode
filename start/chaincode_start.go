b4246cdabd8075b17b66ca90ced7ec4b8d63cb2d
ReshmaSC <reshma.choukimath@in.ibm.com>
2017-03-19 16:15:06 +0530

    added git fucntion

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -226,7 +226,24 @@ type SomeStruct struct {
 
 
 
+}
 
 
 
+func (t *SimpleChaincode) readVehicle(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var name, jsonResp string
+    var err error
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+    }
+
+    name = args[0]
+    valAsbytes, err := stub.GetState(name)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
 }
